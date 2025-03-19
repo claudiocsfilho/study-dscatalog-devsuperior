64baf8f43274f0c9ba4f3a9fd1e2a57f8e9ec6a0
Claudio Filho <claudiocsfilho@gmail.com>
2025-03-19 12:34:45 -0300

    Add CategoryDTO, applying FindAll with Page
    Add User and Role Entities

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,9 +1,24 @@
 package com.devsuperior.dscatalog.controllers;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable){
+        return ResponseEntity.ok().body(service.findAll(pageable));
+    }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,12 +1,24 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<CategoryDTO> findAll(Pageable pageable){
+       Page<Category> result = repository.findAll(pageable);
+       Page<CategoryDTO> dto = result.map(CategoryDTO::new);
+        return dto;
+    }
 }
