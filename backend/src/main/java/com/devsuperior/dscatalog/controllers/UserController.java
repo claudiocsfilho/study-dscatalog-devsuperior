e06a5f3c05d0e48ad7232bb219f20d283c9b4adf
Claudio Filho <claudiocsfilho@gmail.com>
2025-04-04 18:57:31 -0300

    Add UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/UserController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.controllers;
 
 import com.devsuperior.dscatalog.dto.UserDTO;
 import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,8 +41,8 @@ public class UserController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> update (@PathVariable Long id, @Valid @RequestBody UserDTO dto){
-        dto = service.update(id, dto);
+    public ResponseEntity<UserDTO> update (@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto){
+        UserDTO newDTO = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
 
